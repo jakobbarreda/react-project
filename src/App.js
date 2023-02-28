e042a7a6ddf335adcb9f2d21bfead082107aacbd
jakob barreda <jakobbarreda@gmail.com>
2023-02-27 21:22:52 -0600

    finished addExpenseHandler

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
+import { useState } from "react";
 import ExpensesList from "./components/Expenses/ExpensesList";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const dummy = [
+  {
+    id: 1,
+    title: "hello world",
+    date: new Date("january 25 2019"),
+    price: 100,
+  },
+  {
+    id: 2,
+    title: "crazy 8's",
+    date: new Date("january 25 2019"),
+    price: 200,
+  },
+  {
+    id: 3,
+    title: "come down",
+    date: new Date("january 25 2019"),
+    price: 132,
+  },
+  {
+    id: 4,
+    title: "pizza on fire",
+    date: new Date("january 25 2019"),
+    price: 332,
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: 1,
-      title: "hello world",
-      date: new Date("january 25 2019"),
-      price: 100,
-    },
-    {
-      id: 2,
-      title: "crazy 8's",
-      date: new Date("january 25 2019"),
-      price: 200,
-    },
-    {
-      id: 3,
-      title: "come down",
-      date: new Date("january 25 2019"),
-      price: 132,
-    },
-    {
-      id: 4,
-      title: "pizza on fire",
-      date: new Date("january 25 2019"),
-      price: 332,
-    },
-  ];
+  const [expenses, setExpenses] = useState(dummy);
 
   const addExpenseHandler = (expense) => {
-    expenses.push(expense);
-    console.log(expenses);
+    setExpenses((prevState) => {
+      return [expense, ...prevState];
+    });
   };
 
   return (
