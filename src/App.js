4b89a5e8e224fb68348bf3639b7fd6f2cbe1b785
jakob barreda <jakobbarreda@gmail.com>
2023-03-01 01:55:04 -0600

    add more dummy data

--- src/App.js
+++ src/App.js
@@ -2,35 +2,47 @@ import { useState } from "react";
 import ExpensesList from "./components/Expenses/ExpensesList";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const dummy = [
+const dummyData = [
   {
     id: 1,
-    title: "hello world",
-    date: new Date("january 25 2020"),
-    price: 100.0,
+    title: "Car Insurance",
+    date: new Date("january 25 2022"),
+    price: 238.0,
   },
   {
     id: 2,
-    title: "crazy 8's",
-    date: new Date("january 25 2019"),
-    price: 200.3,
+    title: "Rent",
+    date: new Date("febuary 05 2022"),
+    price: 885,
   },
   {
     id: 3,
-    title: "come down",
-    date: new Date("january 25 2019"),
-    price: 132.53,
+    title: "Hotel",
+    date: new Date("april 13 2021"),
+    price: 400.0,
   },
   {
     id: 4,
-    title: "pizza on fire",
-    date: new Date("january 25 2019"),
+    title: "Car Payment",
+    date: new Date("may 19 2020"),
     price: 332.54,
+  },
+  {
+    id: 5,
+    title: "Apple",
+    date: new Date("december 14 2020"),
+    price: 1000.0,
+  },
+  {
+    id: 6,
+    title: "WingStop",
+    date: new Date("auguest 20 2022"),
+    price: 60.0,
   },
 ];
 
 function App() {
-  const [expenses, setExpenses] = useState(dummy);
+  const [expenses, setExpenses] = useState(dummyData);
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevState) => {
